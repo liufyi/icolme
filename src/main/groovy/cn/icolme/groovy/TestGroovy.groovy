87c733afd5d4ffcd4dba50d3fa601043f8f464c4
liufyi <443843003@qq.com>
2016-05-01 16:38:14 +0800

    test

---
+++ src/main/groovy/cn/icolme/groovy/TestGroovy.groovy
@@ -0,0 +1,16 @@
+package cn.icolme.groovy
+
+import org.junit.Test
+
+/**
+ * Created by Administrator on 2016/5/1.
+ */
+class TestGroovy
+{
+    @Test
+    public void testMe()
+    {
+        println "Hello Groovy"
+    }
+}
+
