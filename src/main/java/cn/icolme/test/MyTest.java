a004942e5c6b14cb35e4b8c7ae59c2b082a91df8
liufyi <443843003@qq.com>
2016-05-01 16:20:36 +0800

    Initial commit

---
+++ src/main/java/cn/icolme/test/MyTest.java
@@ -0,0 +1,18 @@
+package cn.icolme.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/5/1.
+ */
+public class MyTest
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    @Test
+    public void testMe()
+    {
+        System.out.println("testMe method");
+    }
+}
